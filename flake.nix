af856efdfbb827c71540355808d093ec34f4a968
blezz-tech <markus.jenya04@yandex.ru>
2024-02-03 22:24:25 +0300

    update

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "My custom Docker image";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in
+    {
+      app = pkgs.dockerTools.buildImage {
+        name = "gitea.blezz-tech.ru/blezz-tech/actions";
+        tag = "latest";
+        created = "now";
+        copyToRoot = pkgs.buildEnv {
+          name = "image-root";
+          paths = with pkgs; [
+            bash
+            coreutils-full
+            nushell
+            pandoc
+            vim
+          ];
+          pathsToLink = [ "/bin" ];
+        };
+
+        config.Cmd = [ "/bin/bash" ];
+      };
+    };
+}
